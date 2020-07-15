560a9d7f770586eccbd56ab961a82a8622f85644
gkhnrsln <53009328+gkhnrsln@users.noreply.github.com>
2020-07-15 16:02:39 +0200

    jkui

--- Zinsrechner/testKBUnit/prlab/kbunit/business/ctci/DataCTCI.java
+++ Zinsrechner/testKBUnit/prlab/kbunit/business/ctci/DataCTCI.java
@@ -11,7 +11,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import prlab.kbunit.enums.Variables;
@@ -33,7 +32,6 @@ import prlab.kbunit.test.ClassCreator;
  * @author G&ouml;khan Arslan
  */
 public class DataCTCI {
-	private URI file;
 	/** JUnit Testtyp der Testklasse */
 	private int testTyp;
 	/** Liste der TestMethoden der Testklasse */
@@ -51,269 +49,53 @@ public class DataCTCI {
 	 * @throws IOException 
 	 * @throws ClassNotFoundException 
 	 */
-	public DataCTCI(URI file) throws IOException, ClassNotFoundException {
-		this.file = file;
-		this.testTyp = testType();
-		this.listeTestMethoden = testMethoden();
-		this.listeTestAttribute = testAttribute();
-		this.listeDescTestMethoden = descTestMethoden();
-		this.listeDescTestAttribute = descTestAttribute();
-	}
-	
-	/**
-	 * Gibt eine Liste der Javadoc-Kommentare zu den Testmethoden der
-	 * angegebenen Java Datei zur&uuml;ck.
-	 * @param file Dateipfad der Testklasse
-	 * @return Liste der Beschreibungen zu den Testmethoden
-	 * @exception IOException
-	 */
-	private List<String> descTestMethoden() {
-		//Temporaere Liste fuer Zeileninhalte
-		List<String> tmp = new ArrayList<String>();
-		//Liste mit den Beschreibungen zu den Testmethoden
-		List<String> liste = new ArrayList<String>();
-		
-		//Lese file zeilenweise aus und fuege Zeile in eine Liste
-		BufferedReader in;
-		try {
-			File f = new File(file);
-			in = new BufferedReader(new FileReader(f));
-			String s = in.readLine();
-			tmp.add(s);
-			while(s != null) {
-				s = s.trim();
-				tmp.add(s); 
-				s = in.readLine();
-			}
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		int i = 0; //Zaehler fuer Zeilen (abwaerts)
-		
-		// gehe jede Zeile abwaerts durch
-		while (i < tmp.size()) {
-			//Javadoc-Kommentare der TestMethoden
-			String strDesc = "";
-			String line = tmp.get(i); //aktuelle Zeile
-			//falls aktuelle Zeile eine Testmethode ist
-			if(line.contains("void test") 
-				|| line.contains(Variables.DYNAMIC_NODE) 
-				|| line.contains(Variables.DYNAMIC_TEST)
-				|| line.contains(Variables.DYNAMIC_CONTAINER)
-				) {
-				//Zaehler fuer Zeilen (aufwaerts)
-				int j = 1;
-				//pruefe bis in vorheriger Zeile "/**" vorkommt
-				while (!tmp.get(i-j).startsWith("/**")) {
-					String prevLine = tmp.get(i-j);
-					//wenn vorherige Zeile mit "}" oder ";" endet 
-					//-> kein Javadoc-Kommentar vorhanden
-					if (prevLine.endsWith("}") || (prevLine.endsWith(";") 
-							&& ! prevLine.startsWith("*"))) {
-						strDesc = "Beschreibung fehlt"; break;
-					} 
-					//Zeile ueberspringen wenn Annotation / Javadoc Ende vorhanden ist
-					else if(prevLine.startsWith("@") || prevLine.contains("*/")) {
-						j++; //naechste Zeile (aufwaerts)
-						continue;
-					}
-					//wenn Javadoc-Tag vorhanden
-					else if(prevLine.contains("* @")) {
-						strDesc = ""; //vorherigen Inhalt entfernen
-						j++; //naechste Zeile (aufwaerts)
-						continue;
-					}
-					else {
-						strDesc = prevLine + strDesc;
-						j++; //naechste Zeile (aufwaerts)
-					}
-				}
-				//letzte Formatierungen
-				strDesc = strDesc.replace("*", "").replace("[:ss]", "ß")
-						.replace("[:A]", "Ä").replace("[:O]", "Ö")
-						.replace("[:U]", "Ü").replace("[:a]", "ä")
-						.replace("[:o]", "ö").replace("[:u]", "ü");
-				//fuege der Liste ein neue Beschreibung hinzu
-				liste.add(strDesc);
-			}
-			i++;//naechste Zeile (abwaerts)
-		}
-		return liste;
-	}
-	/**
-	 * Gibt eine Liste der Javadoc-Kommentare zu den Testattributen der 
-	 * angegebenen Java Datei zur&uuml;ck.
-	 * @param file Pfad der Testklasse
-	 * @return Liste der Beschreibungen zu den Testattributen
-	 * @exception IOException
-	 */
-	private List<String> descTestAttribute() {
-		//Temporaere Liste fuer Zeileninhalte
-		List<String> tmp = new ArrayList<String>();
-		//Liste mit den Beschreibungen zu den Testattributen
-		List<String> liste = new ArrayList<String>();
-		
-		//Lese file zeilenweise aus und fuege Zeile in eine Liste
-		BufferedReader in;
-		try {
-			File f = new File(file);
-			in = new BufferedReader(new FileReader(f));
-			String s = in.readLine();
-			tmp.add(s);
-			while(s != null) {
-				s = s.trim();
-				tmp.add(s); 
-				s = in.readLine();
-			}
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		int i = 0; //Zaehler fuer Zeilen (abwaerts)
-		
-		//gehe jede Zeile durch (abwaerts)
-		while (i < tmp.size()) {
-			//Javadoc-Kommentar der Testattribute
-			String strDesc = "";
-			String line = tmp.get(i); //aktuelle Zeile
-			if(line.startsWith("public static") && line.contains("test")) {
-				int j = 1; //Zaehler fuer Zeilen (aufwaerts)
-
-				//pruefe ob Beschreibung vorhanden
-				do {
-					String prevLine = tmp.get(i-j); //vorherhiger Zeileninhalt
-					if (prevLine.startsWith("/**")) {
-						strDesc = prevLine + strDesc; break;
-					}
-					else if (prevLine.endsWith(";")) {
-						strDesc = "Beschreibung fehlt"; break;
-					}
-					strDesc = prevLine + strDesc;
-					j++; //naechste Zeile (aufwaerts)
-				} while (!tmp.get(i-j).endsWith("*/"));
-				//letzte Formatierungen
-				strDesc = strDesc.substring(2, strDesc.indexOf("*/"));
-				strDesc = strDesc.replace("*", "")
-						//Javadoc-Tags entfernen
-						//.replace("{@\\w+.$", "")
-						.replace("{@link", "")
-					    .replace("{@code", "")
-						.replace("}", "")
-						//Umlaute
-						.replace("[:ss]", "ß").replace("[:A]", "Ä")
-						.replace("[:O]", "Ö").replace("[:U]", "Ü")
-						.replace("[:a]", "ä").replace("[:o]", "ö")
-						.replace("[:u]", "ü");
-				//fuege der Liste ein neue Beschreibung hinzu
-				liste.add(strDesc);
-			}
-			i++; //naechste Zeile (abwaerts)
-		}
-		return liste;
-	} 
-	
-	/**
-	 * Gibt JUnit Testtyp der &uuml;bergebenen Java Testklasse zur&uuml;ck.
-	 * @param file Datei, dessen JUnit-Testtyp (4 oder 5) erkannt werden soll
-	 * @return Entweder {@code 5} oder {@code 4}.
-	 * @exception IOException
-	 */
-	private int testType() throws IOException {
-		Stream<String> testTyp;
-		try (Stream<String> stream = Files.lines(Paths.get(file))) {
-			testTyp = stream
-					.map(Objects::toString)
-					//JUnit 5 enthaelt immer folgendes import
-					.filter(type -> type.contains("import org.junit.jupiter"));
-			return testTyp.count() > 0 ? 5 : 4;
-		}
-	}
-	
-	/**
-	 * Gibt eine Liste mit jeder Testmethode der angegebenen Datei zur&uuml;ck.
-	 * @param file Datei, dessen Methoden gelistet werden sollen.
-	 * @return Liste der Testmethoden
-	 * @exception ClassNotFoundException
-	 */
-	private List<String> testMethoden() throws IOException {
-		List<String> liste = null;
-		List<String> methoden = new ArrayList<>();
-		
-		//lies die Datei zeilenweise aus
-		try (Stream<String> stream = Files.lines(Paths.get(file))) {
-			liste = stream
-					.map(Objects::toString)
-					.filter(m -> m.contains(Variables.METHOD_START_VOID)
-						|| m.contains(Variables.DYNAMIC_NODE)
-						|| m.contains(Variables.DYNAMIC_CONTAINER)
-						|| m.contains(Variables.DYNAMIC_TEST)
-						|| m.endsWith(Variables.ANNOTATION_TEST5) 
-						|| m.contains(Variables.ANNOTATION_TEST5_REPEATED) 
-						|| m.contains(Variables.ANNOTATION_TEST5_PARAMETERIZED)
-						|| m.contains(Variables.ANNOTATION_TEST5_FACTORY)
-						|| m.contains(Variables.ANNOTATION_TEST5_TEMPLATE)
-						)
-					.collect(Collectors.toList());
-		}
-		
-		for (int i = 0; i<liste.size(); i++) {
-			String line = liste.get(i);
-			if (line.contains(Variables.ANNOTATION_TEST5)
-				|| line.contains(Variables.ANNOTATION_TEST5_PARAMETERIZED)
-				|| line.contains(Variables.ANNOTATION_TEST5_REPEATED)
-				|| line.contains(Variables.ANNOTATION_TEST5_FACTORY)
-				|| line.contains(Variables.ANNOTATION_TEST5_TEMPLATE)
-				) {
-				String methode = liste.get(i+1)
-						.substring(liste.get(i+1).indexOf("test"));
-				methoden.add(methode.substring(0, methode.indexOf('(')));
-			}
-		}
-		return methoden;
-	}
-	
-	/**
-	 * Gibt eine Liste mit jedem Testattribut in der angegebenen Datei zur&uuml;ck.
-	 * @param file Datei, dessen Attribute gelistet werden sollen
-	 * @return Liste der Testattribute
-	 * @exception IOException
-	 */
-	private List<String> testAttribute() throws ClassNotFoundException {
-		//Formatierungen fuer den Pfad
-		File file = new File(this.file);
-		String strFile = ClassCreator.convertIntoClassName(file, Variables.TEST_SOURCE);
-		
-		List<String> liste = new ArrayList<>();
-
-		Class<?> clazz = Class.forName(strFile);
-		for (Field field : clazz.getFields()) {
-			if(field.getName().startsWith("test"))
-				liste.add(field.getName());
-		}
-		return liste;
+	public DataCTCI(int typ, List<String> listeAttr, List<String> listeMeth, List<String> listeDescAttr, List<String> listeDescMeth) 
+			throws IOException, ClassNotFoundException {
+		this.testTyp = typ;
+		this.listeTestAttribute = listeAttr;
+		this.listeTestMethoden = listeMeth;
+		this.listeDescTestMethoden = listeDescMeth;
+		this.listeDescTestAttribute = listeDescAttr;
 	}
 
-	//getter
+	//getter setter
 	public int getTestTyp() {
 		return testTyp;
+	}
+	
+	public void setTestTyp(int typ) {
+		this.testTyp = typ;
 	}
 
 	public List<String> getListeTestMethoden() {
 		return listeTestMethoden;
 	}
-
+	
+	public void setListeTestMethoden(List<String> liste) {
+		this.listeTestMethoden = liste;
+	}
+	
 	public List<String> getListeDescTestMethoden() {
 		return listeDescTestMethoden;
+	}
+	
+	public void setListeDescTestMethoden(List<String> liste) {
+		this.listeDescTestMethoden = liste;
 	}
 
 	public List<String> getListeTestAttribute() {
 		return listeTestAttribute;
 	}
-
+	
+	public void setListeTestAttribute(List<String> liste) {
+		this.listeTestAttribute = liste;
+	}
+	
 	public List<String> getListeDescTestAttribute() {
 		return listeDescTestAttribute;
+	}
+	
+	public void setListeDescTestAttribute(List<String> liste) {
+		this.listeDescTestAttribute = liste;
 	}
 }
--- Zinsrechner/testKBUnit/prlab/kbunit/file/FileCreatorCTCI.java
+++ Zinsrechner/testKBUnit/prlab/kbunit/file/FileCreatorCTCI.java
@@ -1,11 +1,20 @@
 package prlab.kbunit.file;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -15,6 +24,7 @@ import org.jdom2.output.XMLOutputter;
 import prlab.kbunit.business.ctci.DataCTCI;
 import prlab.kbunit.enums.Variables;
 import prlab.kbunit.scan.FolderScanner;
+import prlab.kbunit.test.ClassCreator;
 
 /*
  * TODO: Soll Model Klasse werden.
@@ -110,6 +120,239 @@ public class FileCreatorCTCI {
 	}
 	
 	/**
+	 * Gibt eine Liste der Javadoc-Kommentare zu den Testmethoden der
+	 * angegebenen Java Datei zur&uuml;ck.
+	 * @param file Dateipfad der Testklasse
+	 * @return Liste der Beschreibungen zu den Testmethoden
+	 * @exception IOException
+	 */
+	private List<String> descTestMethoden(File file) {
+		//Temporaere Liste fuer Zeileninhalte
+		List<String> tmp = new ArrayList<String>();
+		//Liste mit den Beschreibungen zu den Testmethoden
+		List<String> liste = new ArrayList<String>();
+		
+		//Lese file zeilenweise aus und fuege Zeile in eine Liste
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new FileReader(file));
+			String s = in.readLine();
+			tmp.add(s);
+			while(s != null) {
+				s = s.trim();
+				tmp.add(s); 
+				s = in.readLine();
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		int i = 0; //Zaehler fuer Zeilen (abwaerts)
+		
+		// gehe jede Zeile abwaerts durch
+		while (i < tmp.size()) {
+			//Javadoc-Kommentare der TestMethoden
+			String strDesc = "";
+			String line = tmp.get(i); //aktuelle Zeile
+			//falls aktuelle Zeile eine Testmethode ist
+			if(line.contains("void test") 
+				|| line.contains(Variables.DYNAMIC_NODE) 
+				|| line.contains(Variables.DYNAMIC_TEST)
+				|| line.contains(Variables.DYNAMIC_CONTAINER)
+				) {
+				//Zaehler fuer Zeilen (aufwaerts)
+				int j = 1;
+				//pruefe bis in vorheriger Zeile "/**" vorkommt
+				while (!tmp.get(i-j).startsWith("/**")) {
+					String prevLine = tmp.get(i-j);
+					//wenn vorherige Zeile mit "}" oder ";" endet 
+					//-> kein Javadoc-Kommentar vorhanden
+					if (prevLine.endsWith("}") || (prevLine.endsWith(";") 
+							&& ! prevLine.startsWith("*"))) {
+						strDesc = "Beschreibung fehlt"; break;
+					} 
+					//Zeile ueberspringen wenn Annotation / Javadoc Ende vorhanden ist
+					else if(prevLine.startsWith("@") || prevLine.contains("*/")) {
+						j++; //naechste Zeile (aufwaerts)
+						continue;
+					}
+					//wenn Javadoc-Tag vorhanden
+					else if(prevLine.contains("* @")) {
+						strDesc = ""; //vorherigen Inhalt entfernen
+						j++; //naechste Zeile (aufwaerts)
+						continue;
+					}
+					else {
+						strDesc = prevLine + strDesc;
+						j++; //naechste Zeile (aufwaerts)
+					}
+				}
+				//letzte Formatierungen
+				strDesc = strDesc.replace("*", "").replace("[:ss]", "ß")
+						.replace("[:A]", "Ä").replace("[:O]", "Ö")
+						.replace("[:U]", "Ü").replace("[:a]", "ä")
+						.replace("[:o]", "ö").replace("[:u]", "ü");
+				//fuege der Liste ein neue Beschreibung hinzu
+				liste.add(strDesc);
+			}
+			i++;//naechste Zeile (abwaerts)
+		}
+		return liste;
+	}
+	/**
+	 * Gibt eine Liste der Javadoc-Kommentare zu den Testattributen der 
+	 * angegebenen Java Datei zur&uuml;ck.
+	 * @param file Pfad der Testklasse
+	 * @return Liste der Beschreibungen zu den Testattributen
+	 * @exception IOException
+	 */
+	private List<String> descTestAttribute(File file) {
+		//Temporaere Liste fuer Zeileninhalte
+		List<String> tmp = new ArrayList<String>();
+		//Liste mit den Beschreibungen zu den Testattributen
+		List<String> liste = new ArrayList<String>();
+		
+		//Lese file zeilenweise aus und fuege Zeile in eine Liste
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new FileReader(file));
+			String s = in.readLine();
+			tmp.add(s);
+			while(s != null) {
+				s = s.trim();
+				tmp.add(s); 
+				s = in.readLine();
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		int i = 0; //Zaehler fuer Zeilen (abwaerts)
+		
+		//gehe jede Zeile durch (abwaerts)
+		while (i < tmp.size()) {
+			//Javadoc-Kommentar der Testattribute
+			String strDesc = "";
+			String line = tmp.get(i); //aktuelle Zeile
+			if(line.startsWith("public static") && line.contains("test")) {
+				int j = 1; //Zaehler fuer Zeilen (aufwaerts)
+
+				//pruefe ob Beschreibung vorhanden
+				do {
+					String prevLine = tmp.get(i-j); //vorherhiger Zeileninhalt
+					if (prevLine.startsWith("/**")) {
+						strDesc = prevLine + strDesc; break;
+					}
+					else if (prevLine.endsWith(";")) {
+						strDesc = "Beschreibung fehlt"; break;
+					}
+					strDesc = prevLine + strDesc;
+					j++; //naechste Zeile (aufwaerts)
+				} while (!tmp.get(i-j).endsWith("*/"));
+				//letzte Formatierungen
+				strDesc = strDesc.substring(2, strDesc.indexOf("*/"));
+				strDesc = strDesc.replace("*", "")
+						//Javadoc-Tags entfernen
+						//.replace("{@\\w+.$", "")
+						.replace("{@link", "")
+					    .replace("{@code", "")
+						.replace("}", "")
+						//Umlaute
+						.replace("[:ss]", "ß").replace("[:A]", "Ä")
+						.replace("[:O]", "Ö").replace("[:U]", "Ü")
+						.replace("[:a]", "ä").replace("[:o]", "ö")
+						.replace("[:u]", "ü");
+				//fuege der Liste ein neue Beschreibung hinzu
+				liste.add(strDesc);
+			}
+			i++; //naechste Zeile (abwaerts)
+		}
+		return liste;
+	} 
+	
+	/**
+	 * Gibt eine Liste mit jedem Testattribut in der angegebenen Datei zur&uuml;ck.
+	 * @param file Datei, dessen Attribute gelistet werden sollen
+	 * @return Liste der Testattribute
+	 * @exception IOException
+	 */
+	private List<String> testAttribute(File file) throws ClassNotFoundException {
+		//Formatierungen fuer den Pfad
+		String strFile = ClassCreator.convertIntoClassName(file, Variables.TEST_SOURCE);
+		
+		List<String> liste = new ArrayList<>();
+
+		Class<?> clazz = Class.forName(strFile);
+		for (Field field : clazz.getFields()) {
+			if(field.getName().startsWith("test"))
+				liste.add(field.getName());
+		}
+		return liste;
+	}
+	
+	/**
+	 * Gibt eine Liste mit jeder Testmethode der angegebenen Datei zur&uuml;ck.
+	 * @param file Datei, dessen Methoden gelistet werden sollen.
+	 * @return Liste der Testmethoden
+	 * @exception ClassNotFoundException
+	 */
+	private List<String> testMethoden(URI file) throws IOException {
+		List<String> liste = null;
+		List<String> methoden = new ArrayList<>();
+		
+		//lies die Datei zeilenweise aus
+		try (Stream<String> stream = Files.lines(Paths.get(file))) {
+			liste = stream
+					.map(Objects::toString)
+					.filter(m -> m.contains(Variables.METHOD_START_VOID)
+						|| m.contains(Variables.DYNAMIC_NODE)
+						|| m.contains(Variables.DYNAMIC_CONTAINER)
+						|| m.contains(Variables.DYNAMIC_TEST)
+						|| m.endsWith(Variables.ANNOTATION_TEST5) 
+						|| m.contains(Variables.ANNOTATION_TEST5_REPEATED) 
+						|| m.contains(Variables.ANNOTATION_TEST5_PARAMETERIZED)
+						|| m.contains(Variables.ANNOTATION_TEST5_FACTORY)
+						|| m.contains(Variables.ANNOTATION_TEST5_TEMPLATE)
+						)
+					.collect(Collectors.toList());
+		}
+		
+		for (int i = 0; i<liste.size(); i++) {
+			String line = liste.get(i);
+			if (line.contains(Variables.ANNOTATION_TEST5)
+				|| line.contains(Variables.ANNOTATION_TEST5_PARAMETERIZED)
+				|| line.contains(Variables.ANNOTATION_TEST5_REPEATED)
+				|| line.contains(Variables.ANNOTATION_TEST5_FACTORY)
+				|| line.contains(Variables.ANNOTATION_TEST5_TEMPLATE)
+				) {
+				String methode = liste.get(i+1)
+						.substring(liste.get(i+1).indexOf("test"));
+				methoden.add(methode.substring(0, methode.indexOf('(')));
+			}
+		}
+		return methoden;
+	}
+	
+	/**
+	 * Gibt JUnit Testtyp der &uuml;bergebenen Java Testklasse zur&uuml;ck.
+	 * @param file Datei, dessen JUnit-Testtyp (4 oder 5) erkannt werden soll
+	 * @return Entweder {@code 5} oder {@code 4}.
+	 * @exception IOException
+	 */
+	private int testType(URI file) throws IOException {
+		Stream<String> testTyp;
+		try (Stream<String> stream = Files.lines(Paths.get(file))) {
+			testTyp = stream
+					.map(Objects::toString)
+					//JUnit 5 enthaelt immer folgendes import
+					.filter(type -> type.contains("import org.junit.jupiter"));
+			return testTyp.count() > 0 ? 5 : 4;
+		}
+	}
+	
+	/**
 	 * Erstellt die CustomerTestCaseInformation f&uuml;r eine bestimmte Testklasse,
 	 * die vom Anwender in KBUnit-Entwickler ausgew&auml;hlt wurde.
 	 * @param index gibt die Position des ausgew&auml;hlten Elements der 
@@ -135,8 +378,13 @@ public class FileCreatorCTCI {
 				.replace("\\", ".")
 				.substring(5, strKlasse.indexOf(Variables.EXTENSION_JAVA));
 		
-		DataCTCI testKlasse = new DataCTCI(listeKlassen.get(index).toURI());
-		int testType = testKlasse.getTestTyp();
+		DataCTCI testKlasse = new DataCTCI(
+				testType(listeKlassen.get(index).toURI()),
+				testAttribute(listeKlassen.get(index)),
+				testMethoden(listeKlassen.get(index).toURI()),
+				descTestAttribute(listeKlassen.get(index)),
+				descTestMethoden(listeKlassen.get(index))
+				);
 		int k = 0; //Zaehler fuer testMethoden
 		
 		//Gehe jede Testmethode der Testklasse durch
@@ -168,7 +416,7 @@ public class FileCreatorCTCI {
 			
 			elTestCase.setContent(elPath.setText(strKlasse + "." + strMeth))
 			   	.addContent(elDescMethode.clone().setText(strDescMeth))
-			   	.addContent(elTestType.clone().setText("" + testType))
+			   	.addContent(elTestType.clone().setText("" + testKlasse.getTestTyp()))
 			   	.addContent(elParameters.clone()
 			);
 				
@@ -202,9 +450,15 @@ public class FileCreatorCTCI {
 			strKlasse = strKlasse
 					.replace("\\", ".")
 					.substring(5, strKlasse.indexOf(Variables.EXTENSION_JAVA));
-			
-			DataCTCI testKlasse = new DataCTCI(listeKlassen.get(i).toURI());
-			int testType = testKlasse.getTestTyp();
+		
+			DataCTCI testKlasse = new DataCTCI(
+					testType(listeKlassen.get(i).toURI()),
+					testAttribute(listeKlassen.get(i)),
+					testMethoden(listeKlassen.get(i).toURI()),
+					descTestAttribute(listeKlassen.get(i)),
+					descTestMethoden(listeKlassen.get(i))
+					);
+		
 			int k = 0; //Zaehler fuer testMethoden
 			boolean isNextClass = true;
 			
@@ -242,7 +496,7 @@ public class FileCreatorCTCI {
  
 				elTestCase.setContent(elPath.setText(strKlasse + "." + strMeth))
 					.addContent(elDescMethode.clone().setText(strDescMeth))
-					.addContent(elTestType.clone().setText(""+ testType))
+					.addContent(elTestType.clone().setText(""+ testKlasse.getTestTyp()))
 					.addContent(elParameters.clone()
 				);
 				
