30f5e9c13d618582fc4293a6dd8ea4dbe25e0934
gkhnrsln <53009328+gkhnrsln@users.noreply.github.com>
2020-08-15 00:27:56 +0200

    k

--- Zinsrechner/testPlain/darlehen/TilgungsdarlehenTestPlain.java
+++ Zinsrechner/testPlain/darlehen/TilgungsdarlehenTestPlain.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author Philipp Sprengholz
 */
 
-class Tilgungsdarlehen2TestPlain {
+class TilgungsdarlehenTestPlain {
 	// das zu testende Tilgungsdarlehen-Objekt
 	private Tilgungsdarlehen t;
 
